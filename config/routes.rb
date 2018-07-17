61bc72c7f8a86f6fc4492aeaa30b6ad0cdd8a06a
Geoff Humphreys <humphreys.geoff@gmail.com>
2018-07-17 15:37:20 -0400

    Set up Sendgrid with heroku, adjusted style for responsive, updated links and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :contacts, only: [:new, :create]
+  get 'contact', to:  'contacts#new'
   get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
   get 'faqs', to: 'pages#faqs'
-
   root to: 'pages#home'
 end
